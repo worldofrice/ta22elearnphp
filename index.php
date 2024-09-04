5b979d28d661a58d13238566de4da25bb063d6ac
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-04 08:58:39 +0300

    Pointers

--- index.php
+++ index.php
@@ -1,22 +1,14 @@
 <?php
-$name = 'world';
-$string = "hello\n $name";
 
-var_dump($string);
+$array = [1,2,3,4,5];
 
+for($i=0;$i<count($array);$i++){
+    $value = &$array[$i];
 
-
-$array = [
-    'num1' => 1,
-    'num2' => 2,
-    'num3' => 3
-];
-
-foreach($array as $value){
-    
 }
 
-foreach($array as $key=>$value){
-    echo "$key=>$value\n";
+foreach($array as &$value){
+    $value *= 2;
+    var_dump($value);
 }
-
+var_dump($array);
