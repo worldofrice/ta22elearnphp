4226432fb535fe0566d5357e18241eff35832123
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-04 09:53:18 +0300

    Interface

--- index.php
+++ index.php
@@ -1,44 +1,31 @@
 <?php
 
-class Box {
-    private $width;
-    protected $height;
-    public int $length;
-
-    public function getWidth(){
-        return $this->width . ' meters';
+class ConsoleLogger implements Logger {
+    public function log($message){
+        echo "$message\n";
     }
-    public function setWidth($width){
-        if($width < 0){
-            $width = 0;
+}
+
+class Work {
+    public function task(Logger $logger){
+        for($i=0;$i<10;$i++){
+            $logger->log($i);
         }
-        $this->width = $width;
     }
 }
 
-class MetalBox extends Box {
-    use HasMaterial;
-    public function echoWidth(){
-        var_dump($this->width);
-    }
-    public function echoHeight(){
-        var_dump($this->height);
-    }
+interface Logger {
+    public function log($message);
 }
 
-class Ball {
-    use HasMaterial;
-}
 
-trait HasMaterial {
-    public $material;
-    public function setMaterial($material){
-        $this->material = $material;
+//
+
+class NothingLogger implements Logger{
+    public function log($message){
+       
     }
 }
-
-$box = new MetalBox();
-$box->setWidth(-10);
-$box->echoHeight();
-$box->setMaterial('wood');
-var_dump($box);
+$logger = new NothingLogger();
+$work = new Work();
+$work->task($logger);
