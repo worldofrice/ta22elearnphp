dfc93adab8a7e06c572e466cc74153c5b2e449fb
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-09-04 08:46:07 +0300

    Arrays

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
+$name = 'world';
+$string = "hello\n $name";
 
-$i = 1;
-$i = 1.2;
-$i = 'a';
-var_dump($i);
+var_dump($string);
+
+
+
+$array = [
+    'num1' => 1,
+    'num2' => 2,
+    'num3' => 3
+];
+
+foreach($array as $value){
+    
+}
+
+foreach($array as $key=>$value){
+    echo "$key=>$value\n";
+}
+
